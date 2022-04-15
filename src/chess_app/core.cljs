31d01d6ad9c3760fedbff9b5b93324cca9e1b095
alachiq <aaj.joshani@gmail.com>
2022-04-15 11:02:37 +0200

    I broke even more

--- src/chess_app/core.cljs
+++ src/chess_app/core.cljs
@@ -16,52 +16,7 @@
   (and (= figure "P") (= player 0))
   true)
 
-(defn board-view []
-  (let [{:keys [board current-drag]} @app-state]
-    (into
-      [:div {:style {:width 500 :height 500 :border " 1px solid gray"}}]
-      (map-indexed
-        (fn [i row]
-          (into
-            [:div
-             {:style {:height      (/ 500 8)
-                      :display     "flex"
-                      :font-family "sans-serif"
-                      :font-size   "18px"
-                      :background  (if (odd? i) "white" "gray")}}]
-            (map-indexed
-              (fn [j {:keys [player figure highlight?]}]
-                (let [bg-color (if (odd? j) (if (odd? i) "gray" "white")
-                                            (if (odd? i) "white" "gray"))]
-                  [:div
-                   {:on-drag-over  (fn [event]
-                                     #_"TODO: make this only if is-valid?"
-                                     (.preventDefault event)
-                                     (.stopPropagation event)
-                                     (swap! app-state assoc-in [:board i j :highlight?] true))
-                    :on-drag-leave (fn [event] (swap! app-state assoc-in [:board i j :highlight?] false))
-                    :on-drop       (fn [event]
-                                     (let [dragging-figure (get-in @app-state current-drag)]
-                                       (when (and
-                                               (not= current-drag [:board i j])
-                                               (is-valid? dragging-figure current-drag [:board i j]))
-                                         (swap! app-state assoc-in [:board i j] dragging-figure)
-                                         (swap! app-state assoc-in current-drag nil))))
-                    :style         {:width           (/ 500 8)
-                                    :display         "flex"
-                                    :align-items     "center"
-                                    :justify-content "center"
-                                    :background      (if highlight? "yellow" bg-color)}}
-                   (when figure
-                     [:img {:src           (str "/" figure "-" (if (zero? player) "white" "dark") ".svg")
-                            :style         {:cursor "move"}
-                            :draggable     true
-                            :on-drag-start #(do
-                                              (swap! app-state assoc :current-drag [:board i j])
-                                              (.. % -dataTransfer (setData "text/plain" nil)))
-                            :on-drag-end   #(swap! app-state dissoc :current-drag)}])]))
-              row)))
-        board))))
+
 
 (defn user-input []
     [:div {:style {:border " 1px solid black"}}
